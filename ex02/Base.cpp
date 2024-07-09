fa9c1d30976d349a28f61b69fabd386dc9c1300c
Rmehadje <rayanmehadjeri6@gmail.com>
2024-07-09 10:58:26 +0200

    finished ex02

--- ex02/Base.cpp
+++ ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: rmehadje <rmehadje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 14:59:58 by rmehadje          #+#    #+#             */
-/*   Updated: 2024/07/08 16:24:41 by rmehadje         ###   ########.fr       */
+/*   Updated: 2024/07/09 10:57:25 by rmehadje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,46 @@ Base	*make_c(){
 	return new C();
 }
 
-void		*Base::generate(){
+Base		*generate(){
 	Base *(*f[3])() = {make_a, make_b, make_c};
-	srand(time(0));
-	return f[rand() % 3];
+	srand(static_cast<unsigned int>(time(0)));
+	return f[rand() % 3]();
 }
 
-void	Base::identify(Base* p){
+void	identify(Base* p){
 	if (dynamic_cast<A*>(p))
 		std::cout << "A" << std::endl;
-	if (dynamic_cast<B*>(p))
+	else if (dynamic_cast<B*>(p))
 		std::cout << "B" << std::endl;
-	if (dynamic_cast<C*>(p))
+	else if (dynamic_cast<C*>(p))
 		std::cout << "C" << std::endl;
+	else{
+		std::cout << "Not found." << std::endl;
+	}
 }
 
-void		Base::identify(Base &p){}
+void		identify(Base &p){
+	try{
+		A &a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "A" << std::endl;
+	}
+	catch(std::exception &e){
+		try{
+			B &b = dynamic_cast<B&>(p);
+			(void)b;
+			std::cout << "B" << std::endl;
+			}
+		catch(std::exception &e){
+			try{
+				C &c = dynamic_cast<C&>(p);
+				(void)c;
+				std::cout << "C" << std::endl;
+			}
+			catch(std::exception &e)
+			{
+				std::cout << "not found" << std::endl;
+			}
+		}
+	}
+}
--- ex02/Base.hpp
+++ ex02/Base.hpp
@@ -6,24 +6,24 @@
 /*   By: rmehadje <rmehadje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 14:32:45 by rmehadje          #+#    #+#             */
-/*   Updated: 2024/07/08 15:15:53 by rmehadje         ###   ########.fr       */
+/*   Updated: 2024/07/09 10:51:11 by rmehadje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
-#include <iomanip>
+#include <cstdlib>
 #include <ctime>
 
 
 class Base{
 	public:
 	virtual	~Base();
-	void		*generate();
-	void		identify(Base* p);
-	void		identify(Base& p);
 };
 
+void		identify(Base* p);
+void		identify(Base& p);
+Base		*generate();
 class A : public Base {};
 class B : public Base {};
 class C : public Base {};
